0df0993a346dbe32924fb3a31c9d1ff1d807464d
Ferfdz1995 <fernandafdz03@gmail.com>
2018-06-28 20:39:10 -0600

    Actualización de Modelo

--- WiserSoft.UI/Models/Contactos.cs
+++ WiserSoft.UI/Models/Contactos.cs
@@ -7,9 +7,14 @@ namespace WiserSoft.UI.Models
 {
     public class Contactos
     {
+        [Required]
         public int Numero { get; set; }
+        [Required]
         public String Nombre { get; set; }
+        [Required]
         public String Detalle { get; set; }
+        [Required]
+        [EmailAddress]
         public String Correo { get; set; }
         public String Username { get; set; }
     }
