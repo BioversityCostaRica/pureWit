0a7f19072cee56a20a7101042f1f32ead368f6a1
bmadriz <b.madriz@cgiar.org>
2018-07-13 18:00:49 -0600

    Agregando validaciones, llamadas y correo.

--- WiserSoft.UI/Models/Difusiones.cs
+++ WiserSoft.UI/Models/Difusiones.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +9,19 @@ namespace WiserSoft.UI.Models
     public class Difusiones
     {
         public int Id_Difusion { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Debe escribir la descripción del mensaje.")]
         public String Descripcion { get; set; }
         public DateTime Fecha { get; set; }
         public String Username { get; set; }
         public DateTime Fecha_Activacion { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Seleccione el tipo de mensaje que desea enviar.")]
         public int Id_Tipo_Mensaje { get; set; }
         public int Id_Estado { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Seleccione el mensaje que desea enviar.")]
         public int Id_Mensaje { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Seleccione la lista a la que desea enviar el mensaje.")]
         public int Id_Lista { get; set; }
     }
 }
