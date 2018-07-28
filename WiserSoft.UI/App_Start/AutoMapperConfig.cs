9c23df8938c55cde251b70672998d98d801d8dc4
Ferfdz1995 <fernandafdz03@gmail.com>
2018-07-27 21:10:21 -0600

    Update AutoMapperConfig.cs

--- WiserSoft.UI/App_Start/AutoMapperConfig.cs
+++ WiserSoft.UI/App_Start/AutoMapperConfig.cs
@@ -40,7 +40,8 @@ namespace WiserSoft.UI.App_Start
                 cfg.CreateMap<DATA.Mensajes, Models.Mensajes>();
                 cfg.CreateMap<Models.Lista_Negra, DATA.Lista_Negra>();
                 cfg.CreateMap<DATA.Lista_Negra, Models.Lista_Negra>();
-
+                cfg.CreateMap<Models.Respuestas, DATA.Respuestas>();
+                cfg.CreateMap<DATA.Respuestas, Models.Respuestas>();
             });
         }
     }
