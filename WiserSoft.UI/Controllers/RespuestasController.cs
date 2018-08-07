62c4f548d5edb92b452a25f304f3ac3bb3fd8b6c
bmadriz <b.madriz@cgiar.org>
2018-08-06 18:04:07 -0600

    Agregando respuestas y ajustes de incluidos

--- WiserSoft.UI/Controllers/RespuestasController.cs
+++ WiserSoft.UI/Controllers/RespuestasController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 using WiserSoft.DAL.Interfaces;
 using WiserSoft.DAL.Metodos;
 
@@ -13,14 +15,18 @@ namespace WiserSoft.UI.Controllers
     {
         IRespuestas Rest;
         IContactos cont;
+        IComunicaciones com;
+        ITelefonos tel;
         // GET: Respuestas
         public RespuestasController()
         {
             Rest = new MRespuestas();
             cont = new MContactos();
+            com  = new MComunicaciones();
+            tel  = new MTelefonos();
         }
 
-            public ActionResult Index()
+        public ActionResult Index()
         {
             ViewBag.userId = Session["Username"];
 
@@ -37,6 +43,30 @@ namespace WiserSoft.UI.Controllers
             ViewBag.Contacto = Id_Contacto;
             ViewBag.prueba = Session["Username"];
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Mensajes(Models.Comunicaciones comunicaciones)
+        {
+            DATA.Telefonos telefono = new DATA.Telefonos();
+            telefono = tel.ListarTelefonos().Where(x => x.Username == Session["Username"].ToString()).FirstOrDefault();
+            var dateNow = DateTime.Now;
+            comunicaciones.Fecha = dateNow;
+            comunicaciones.Estado = 4;
+            comunicaciones.Numero_Twilio = telefono.Numero;
+            DATA.Contactos contacto = new DATA.Contactos();
+            contacto = cont.BuscarContactos(comunicaciones.Id_Contacto);
+
+            TwilioClient.Init(telefono.Account_Id, telefono.Authtoken);
+            var message = MessageResource.Create(
+                body: comunicaciones.Mensaje,
+                from: new Twilio.Types.PhoneNumber("+" + telefono.Numero),
+                to: new Twilio.Types.PhoneNumber("+" + contacto.Numero)
+            );
+
+            com.InsertarComunicaciones(Mapper.Map<DATA.Comunicaciones>(comunicaciones));
+
+            return RedirectToAction("Mensajes",new { Id_Contacto = comunicaciones.Id_Contacto });
         }
 
         [HttpGet]
